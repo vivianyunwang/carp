b69d12b4fcdd69d27bde8f1a24c677d177f4be01
vivianyunwang <66293081+vivianyunwang@users.noreply.github.com>
2020-08-17 15:20:40 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+/*
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Navigation} from 'react-native-navigation'
+*/
+
 import React, {Component, useState} from 'react';
 import {ImageButton} from 'react-native-image-button-text';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
@@ -6,11 +13,14 @@ import {
     Text,
     View,
     Alert,
-    CheckBox,
     TouchableHighlight,
-    onPress
+    CheckBox
 } from 'react-native';
-import {CustomButton} from './survey/custom-button.js';
+//import {Calendar, Agenda} from 'react-native-calendars';
+//import CalendarList from './calendar-list/CalendarList';
+import {CustomButton} from './custom-button.js';
+import Emoji from './emoji-buttons.js';
+//import t from './survey-data.js';
 
 
 export default function App() {
@@ -101,41 +111,41 @@ export default function App() {
           </View>
           <Text style={styles.questions}>How are you feeling right now? </Text>
           <View style={styles.buttons}>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/1.png')}/>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/2.png')}/>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/3.png')}/>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/4.png')}/>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/5.png')}/>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/6.png')}/>
-            <ImageButton style = {styles.emojiButtons}
-                width={25}
-                height={25}
-                text=""
-                source={require('./survey/emoji-images/7.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./1.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./2.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./3.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./4.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./5.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./6.png')}/>
+            <ImageButton style = {styles.emojiButtons} 
+                width={25} 
+                height={25} 
+                text="" 
+                source={require('./7.png')}/>
           </View>
           <Text style={styles.questions}>How many glasses of water did you drink today? </Text>
           <View style={styles.buttons}>
@@ -196,7 +206,7 @@ export default function App() {
           />
           </View>
           <Text style={styles.questions}>What types of food did you eat today?</Text>
-
+        
               <BouncyCheckbox
               isNotChecked
               textColor="black"
@@ -237,8 +247,8 @@ export default function App() {
               //iconComponent={Icon}
               text="Junk Food 🍿"
             />
-            <TouchableHighlight style={styles.submit}  underlayColor='#99d9f4'>
-          <Text style={{fontSize: '130%', color:'#fff'}}>Save</Text>
+              <TouchableHighlight style={styles.submit}  underlayColor='#99d9f4'>
+          <Text style={styles.questions}>Save</Text>
         </TouchableHighlight>
                   </View>
                   </View>
@@ -248,7 +258,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFBF4B',
+    backgroundColor: 'thistle',
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
@@ -256,8 +266,8 @@ const styles = StyleSheet.create({
 
   },
   container2:{
-    width: 'auto',
-    height: '90%',
+    width: '70%',
+    height: 'auto',
     borderRadius: 20,
     alignItems: 'center',
     justifyContent: 'center',
@@ -270,6 +280,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.30,
     shadowRadius: 20,
     display: "flex",
+
 
   },
   questions:{
@@ -295,14 +306,93 @@ const styles = StyleSheet.create({
   },
   submit:{
     height: 36,
-    backgroundColor: '#002447',
-    borderColor: '#53D3D1',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
     borderWidth: 1,
-    borderRadius: 20,
+    borderRadius: 8,
     marginBottom: 10,
     alignSelf: 'stretch',
-    justifyContent: 'center',
-    textColor: '#fffff',
-    textAlign: 'center'
+    justifyContent: 'center'
   }
 });
+
+/**
+//import ExampleB from './ExampleB';
+//import ExampleC from './ExampleC';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+    },
+    container2: {
+        flex: 1,
+    },
+    bar: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderTopColor: '#fff'
+    },
+    barButton: {
+        flex: 1,
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0.5,
+    },
+    barText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+});
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {selected: 2};
+
+        this.changeExample = this.changeExample.bind(this);
+    }
+
+    /**changeExample() {
+        switch (this.state.selected) {
+            case 0:
+                return <ExampleA/>;
+            case 1:
+                return <ExampleB />;
+            case 2:
+                return <ExampleC />;
+        }
+    }
+
+    render() {
+        return (
+            <View style={[styles.container, {paddingTop: Platform.select({ios: 20, android: 0})}]}>
+              <CalendarList/>
+
+                <View style={{backgroundColor: 'black'}}>
+                    <Text style={styles.barText}>Choose an example</Text>
+                    <View style={styles.bar}>
+                        <TouchableOpacity style={styles.barButton} onPress={() => this.setState({selected: 0})}>
+                            <Text style={styles.barText}>Light</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.barButton} onPress={() => this.setState({selected: 1})}>
+                            <Text style={styles.barText}>Dark</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.barButton} onPress={() => this.setState({selected: 2})}>
+                            <Text style={styles.barText}>Pastel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+**/
